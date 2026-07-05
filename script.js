98826cd7fce7085dc771a6d7c60c624188aa2cd0
abdullah3480 <muhammadabdullahmunir04@gmail.com>
2026-07-05 15:56:44 +0500

    made a button that prompts for no of rows and creates grid but does not remove old grid

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 
+
+const body = document.querySelector("body")
+
+const button = document.createElement("button")
+
+
 const container = document.querySelector("div")
 container.classList.add("container")
 
-let squares = [];
-squares.length = 16*16 +16;
-for(let i = 0; i < 16;i++){
-    squares[i] = document.createElement("div")
-    container.appendChild(squares[i])
-    for(let j = 0 ; j < 16; j++){
-        squares[i * 16 + j + 16] = document.createElement("div")
-        squares[i].appendChild(squares[i * 16 + j + 16])
-        squares[i *16 + j + 16].classList.add("box")
+body.insertBefore(button,container)
+
+button.textContent = "Press to enter No of rows"
+
+let rows = 0;
+button.addEventListener('click', ()=>{
+    rows = prompt()
+    grid.length = rows*rows +rows;
+for(let i = 0; i < rows;i++){
+    grid[i] = document.createElement("div")
+    container.appendChild(grid[i])
+    for(let j = 0 ; j < rows; j++){
+        grid[i * rows + j + rows] = document.createElement("div")
+        grid[i].appendChild(grid[i * rows + j + rows])
+        grid[i *rows + j + rows].classList.add("box")
     }
-    squares[i].classList.add("row")
+    grid[i].classList.add("row")
     
 }
+})
+
+let grid = [];
+
+
 
 
